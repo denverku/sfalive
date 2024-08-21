8edc24c789dda8104e3a660f07e20ac97691012b
denverku <magicph26@gmail.com>
2024-08-21 13:01:20 +0800

    Create config.php

---
+++ api/config.php
@@ -0,0 +1,17 @@
+<?php
+/*
+	Конфигурация сайта
+	Автор - the__all
+*/
+/* Основные */
+$site_url = "127.0.0.1/pb";
+$site_name = "Project SFDraft";
+
+/* База данных */
+$mysqli = mysqli_connect( 
+'localhost', /* Хост, к которому мы подключаемся */ 
+'root', /* Имя пользователя */ 
+'', /* Password to use */ 
+'sfalive'); /* Default query database */
+
+?>
